123674e75d97251de08a8f88f783df8c41b223c4
Miguel4577 <miguelmahos2@gmail.com>
2026-03-02 13:54:29 -0500

    UPDATE RESOURCES

--- models/Resources.js
+++ models/Resources.js
@@ -1,9 +1,64 @@
 class Resources {
-    constructor(amount, type, generatedBy, consumedBy) {
-        this.amount = amount;
+    constructor(generatedAmount, type,consumed) {
         this.type = type;
         this.generatedAmount = 0;
-        this.generatedBy = generatedBy;
-        this.consumedBy = consumedBy;
+        this.consumed = consumed;
+        this.waterAmount=0;
+        this.electricityAmount=0;
+        this.moneyAmount=0;
+        this.foodAmount=0;
+    }
+
+
+    calculateWateramount() {
+        if (this.consumed==true) {
+            this.generatedAmount-= this.waterAmount;
+        }
+        else{
+            if (this.type === "water") {
+                this.generatedAmount += this.waterAmount;
+            }
+    }     
+    
+    return this.electricityAmount;
+
+    }
+    
+     calculateelectricityamount() {
+        if (this.consumed==true) {
+            this.generatedAmount-= this.electricityAmount;
+        }
+        else{
+            if (this.type === "electricity") {
+                this.generatedAmount += this.electricityAmount;
+            }
+        }     
+        return this.electricityAmount;
+
+    }
+
+    calculateMoneyAmount() {
+        if (this.consumed==true) {
+            this.generatedAmount-= this.moneyAmount;
+        }
+        else{
+            if (this.type === "money") {
+                this.generatedAmount += this.moneyAmount;
+            }
+        }
+        return this.moneyAmount;
+    }
+
+    calculateFoodAmount() {
+        if (this.consumed==true) {
+            this.generatedAmount-= this.foodAmount;
+        }
+        else{
+            if (this.type === "food") {
+                this.generatedAmount += this.foodAmount;
+            }
+        }
+        
+        return this.foodAmount;
     }
 }

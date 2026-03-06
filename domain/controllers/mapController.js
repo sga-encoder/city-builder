310d61fe77b1002185475856ca10e32b8d812501
sga-encoder <sga-encoder@users.noreply.github.com>
2026-03-06 08:01:03 -0500

    prueba 3

--- domain/controllers/mapController.js
+++ domain/controllers/mapController.js
@@ -3,7 +3,6 @@ let currentMode = "view"; // "view", "build", "move", "destroy"
 let cameraDidDrag = false;
 
 
-const gar="gay"
 const setupMapCamera = () => {
   const viewport = document.querySelector("#map");
   const map = viewport?.querySelector(".map");
